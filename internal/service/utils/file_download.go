5aabc3332fabfbb37eef6a84b1f8139c690fbe47
bifidokk <bifidokk@gmail.com>
2025-02-13 20:43:00 +0200

    Fix file downloading

--- internal/service/utils/file_download.go
+++ internal/service/utils/file_download.go
@@ -1,49 +1,57 @@
 package utils
 
 import (
-	"errors"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
-	"path"
+	"path/filepath"
 )
 
 func DownloadFileFromURL(fileURL string) (string, error) {
-	parsedURL, err := url.Parse(fileURL)
-	if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
-		return "", errors.New("invalid URL")
-	}
-
-	response, err := http.Get(parsedURL.String())
-	if err != nil {
+	bytes := make([]byte, 8)
+	if _, err := rand.Read(bytes); err != nil {
 		return "", err
 	}
-
-	defer func() {
-		_ = response.Body.Close()
-	}()
-
-	if response.StatusCode != 200 {
-		return "", errors.New("received none 200 response code")
-	}
-
-	fileName := path.Base(parsedURL.Path)
+	randomFileName := hex.EncodeToString(bytes) + ".mp3"
+	filePath := filepath.Join("/tmp", randomFileName)
 
 	// #nosec G304 - Filename is validated
-	file, err := os.CreateTemp("", fileName)
+	file, err := os.Create(filePath)
 	if err != nil {
 		return "", err
 	}
 
 	defer func() {
-		_ = file.Close()
+		err := file.Close()
+		if err != nil {
+			return
+		}
 	}()
 
-	_, err = io.Copy(file, response.Body)
+	// #nosec G107 url is validated
+	resp, err := http.Get(fileURL)
 	if err != nil {
 		return "", err
 	}
 
-	return "/tmp/" + fileName, nil
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			return
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to download file: %s", resp.Status)
+	}
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return filePath, nil
 }
