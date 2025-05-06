1ddcc0d060d92a3060030c7f13710e30acc27c65
bifidokk <bifidokk@gmail.com>
2025-05-06 20:43:04 +0300

    Fix loading env variables

--- internal/config/env.go
+++ internal/config/env.go
@@ -1,14 +1,26 @@
 package config
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
+	"os"
 )
 
 // Load is function to load env variables to os
 func Load(path string) error {
-	err := godotenv.Load(path)
-	if err != nil {
-		return err
+	_ = godotenv.Load(path)
+
+	requiredVars := []string{
+		"TG_BOT_TOKEN",
+		"TIK_TOK_TOKEN",
+		"OPEN_AI_TOKEN",
+		"PG_DSN",
+	}
+
+	for _, v := range requiredVars {
+		if os.Getenv(v) == "" {
+			return fmt.Errorf("required environment variable %s is not set", v)
+		}
 	}
 
 	return nil
